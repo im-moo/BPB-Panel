ab963925aa732a4283812dac97fa1f4ac7361ef1
im-moo <54445570+im-moo@users.noreply.github.com>
2024-09-06 14:36:32 +0800

    Update _worker.js
    8e8c8383-508a-4672-9398-f77cf7666f2c

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '8e8c8383-508a-4672-9398-f77cf7666f2c';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
